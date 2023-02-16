164e7c9fb3250b7100ca82723d1306dab51db617
Esther S. Weon <esther.s.weon@gmail.com>
2023-02-15 22:56:27 -0800

    Update sensor.cpp

--- ros2/transforms/src/sensor.cpp
+++ ros2/transforms/src/sensor.cpp
@@ -4,27 +4,28 @@
 class Sensor: public rclcpp::Node{
 private:
     void objectCallback(){
-        // All transforms must be correctly time-stamped!
+        // All transforms must be correctly timestamped
         pose_.header.stamp = this->get_clock()->now();
         pose_.header.frame_id = "sensor_link";
-        if(count_%2){
+        if (count_%2) {
             pose_.pose.position.x = 1.0;
             pose_.pose.position.y = 1.0;
-        }else{
+        } else {
             pose_.pose.position.x = 2.0;
             pose_.pose.position.y = 3.0;
         }
+        // Change the detected object's position, depending on whether count_ is even or odd
         count_++;
         pose_pub_->publish(pose_);
     }
 
-    // Declare a timer for the simulated detected object
+    // Timer for the simulated detected object
     rclcpp::TimerBase::SharedPtr object_timer_;
-    // Declare a publisher for the simulated detected object
+    // Publisher for the simulated detected object
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub_;
-    // Declare the pose to publish
+    // Pose to publish
     geometry_msgs::msg::PoseStamped pose_;
-    // Aux variable
+    // Aux variable that determines the detected object's position
     int count_ = 0;
 
 public:
@@ -33,7 +34,7 @@ public:
         object_timer_ = create_wall_timer(std::chrono::seconds(1), 
             std::bind(&Sensor::objectCallback, this));
 
-        // Initialize pub for object detection
+        // Initialize publisher for object detection
         pose_pub_ = create_publisher<geometry_msgs::msg::PoseStamped>(
             "/detected_object",10);    
     }
