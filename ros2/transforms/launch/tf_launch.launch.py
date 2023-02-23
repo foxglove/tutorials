dd51a13a5159da6b3f87e6d5926ee249f9029c67
JLBicho <jlmv.96@gmail.com>
2023-02-23 16:46:51 +0100

    Updated space size

--- ros2/transforms/launch/tf_launch.launch.py
+++ ros2/transforms/launch/tf_launch.launch.py
@@ -5,44 +5,44 @@ from launch_ros.actions import Node
 # This function is always needed
 def generate_launch_description():
 
-    arm_frame_publisher = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        arguments=["1", "1", "0", "0", "0", "0", "base_link", "arm_base_link"]
-    )
-    sensor_frame_publisher = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        arguments=["1", "-1", "0", "0", "0", "0", "base_link", "sensor_link"]
-    )
-    tf_broadcaster = Node(
-        package="tf_pkg",
-        executable="tf_broadcaster"
-    )
-    tf_listener = Node(
-        package="tf_pkg",
-        executable="tf_listener"
-    )
-    sensor_node = Node(
-        package="tf_pkg",
-        executable="sensor_node"
-    )
+  arm_frame_publisher = Node(
+    package="tf2_ros",
+    executable="static_transform_publisher",
+    arguments=["1", "1", "0", "0", "0", "0", "base_link", "arm_base_link"]
+  )
+  sensor_frame_publisher = Node(
+    package="tf2_ros",
+    executable="static_transform_publisher",
+    arguments=["1", "-1", "0", "0", "0", "0", "base_link", "sensor_link"]
+  )
+  tf_broadcaster = Node(
+    package="tf_pkg",
+    executable="tf_broadcaster"
+  )
+  tf_listener = Node(
+    package="tf_pkg",
+    executable="tf_listener"
+  )
+  sensor_node = Node(
+    package="tf_pkg",
+    executable="sensor_node"
+  )
 
-    foxglove_bridge_node = Node(
-        package="foxglove_bridge",
-        executable="foxglove_bridge"
-    )
+  foxglove_bridge_node = Node(
+    package="foxglove_bridge",
+    executable="foxglove_bridge"
+  )
 
-    # Launch Foxglove Studio to monitor data
-    foxglove_studio = ExecuteProcess(cmd=["foxglove-studio"])
+  # Launch Foxglove Studio to monitor data
+  foxglove_studio = ExecuteProcess(cmd=["foxglove-studio"])
 
-    # Add the nodes and the process to the LaunchDescription list
-    ld = [arm_frame_publisher,
-          sensor_frame_publisher,
-          tf_broadcaster,
-          tf_listener,
-          sensor_node,
-          foxglove_studio,
-          foxglove_bridge_node]
+  # Add the nodes and the process to the LaunchDescription list
+  ld = [arm_frame_publisher,
+        sensor_frame_publisher,
+        tf_broadcaster,
+        tf_listener,
+        sensor_node,
+        foxglove_studio,
+        foxglove_bridge_node]
 
-    return LaunchDescription(ld)
+  return LaunchDescription(ld)
