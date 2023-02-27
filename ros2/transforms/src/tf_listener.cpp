351e9a96b69a0b597ae4ea47c1ae8f662288027e
Esther S. Weon <esther.s.weon@gmail.com>
2023-02-27 15:58:28 -0800

    Update tf_listener.cpp

--- ros2/transforms/src/tf_listener.cpp
+++ ros2/transforms/src/tf_listener.cpp
@@ -9,31 +9,27 @@
 class TfListener : public rclcpp::Node{
 private:
     void poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg){
-        // IMPORTANT 
-        // Use "try-catch" semantics to avoid problems when a transform does not exists
-        try{
+        try {
             pose_in_ = *msg;
-            // LookupTransform will operate all the matrices between the target_frame and the source_frame
+            // Get all transforms between the source frame and target frame
             tf_buffer_->transform<geometry_msgs::msg::PoseStamped>(pose_in_, pose_out_, "arm_end_link", 
                 tf2::Duration(std::chrono::seconds(1)));
+            // Log coordinates of pose in target frame
             RCLCPP_INFO(get_logger(),"Object pose in 'arm_end_link' is:\n x,y,z = %.1f,%.1f,%.1f",
                 pose_out_.pose.position.x,
                 pose_out_.pose.position.y,
                 pose_out_.pose.position.z);
-        }
-        catch (const tf2::TransformException & ex){
+        } catch (const tf2::TransformException & ex){
             RCLCPP_WARN(get_logger(),"Could not find object position in 'arm_end_link' frame.");
             return;
         }
     }
-    // Subscribe to pose published by sensor node
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
-    // Transform listener
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-    // Transforms buffer
     std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-    // Pose in destination frame
+    // Pose in source frame
     geometry_msgs::msg::PoseStamped pose_in_;
+    // Pose in target frame
     geometry_msgs::msg::PoseStamped pose_out_;
 
 public:
@@ -41,11 +37,11 @@ public:
     Node(name){
         // Load a buffer of transforms
         tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-        // Make a transform listener of the buffer
+        // Listen to the buffer of transforms
         tf_listener_ = std::make_unique<tf2_ros::TransformListener>(*tf_buffer_);
-        // Initialize timer that checks the transforms each second.
+        // Subscribe to pose published by sensor node (check every second)
         pose_sub_ = create_subscription<geometry_msgs::msg::PoseStamped>
-            ("/detected_object",10,
+            ("/detected_object", 10,
             std::bind(&TfListener::poseCallback, this, std::placeholders::_1));
     }
     ~TfListener(){}
