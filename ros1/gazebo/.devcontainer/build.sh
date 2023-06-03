08d1a16ae5734d7a915baeaa2fa0fbc1b1a08b59
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2023-06-02 22:30:06 -0700

    Add initial devcontainer.json

---
+++ ros1/gazebo/.devcontainer/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+cd /home/vscode/catkin_ws
+sudo apt update
+rosdep update
+rosdep install -i --from-path src -y
+source /opt/ros/noetic/setup.bash
+echo "source /opt/ros/noetic/setup.bash" > ~/.bashrc
+catkin_make
+echo "source /home/vscode/catkin_ws/devel/setup.bash" > ~/.bashrc
