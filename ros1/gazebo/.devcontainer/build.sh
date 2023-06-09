424320b45f6047a5c119e1a1c3dab5811b52af3d
Michael Jae-Yoon Chung <mikejaeyoon@gmail.com>
2023-06-09 00:01:28 -0700

    Bug fix

--- ros1/gazebo/.devcontainer/build.sh
+++ ros1/gazebo/.devcontainer/build.sh
@@ -6,6 +6,6 @@ sudo apt update
 rosdep update
 rosdep install -i --from-path src -y
 source /opt/ros/noetic/setup.bash
-echo "source /opt/ros/noetic/setup.bash" > ~/.bashrc
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 catkin_make
-echo "source /home/vscode/catkin_ws/devel/setup.bash" > ~/.bashrc
+echo "source /home/vscode/catkin_ws/devel/setup.bash" >> ~/.bashrc
