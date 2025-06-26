e8d1ed9dde31ea835edb162d02c94aabaa2026b8
Mat Sadowski <msadowski90@gmail.com>
2025-06-26 18:48:25 +0200

    Remove special characters from the rgba-point-cloud

--- studio/rgba-point-cloud/example.py
+++ studio/rgba-point-cloud/example.py
@@ -1,13 +1,13 @@
 import sys
 import math
 import struct
-​
+
 from mcap_ros1.writer import Writer
 from std_msgs.msg import Header
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import PointCloud2, PointField
 from rospy import Time
-​
+
 fields = [
     PointField("x", 0, PointField.FLOAT32, 1),
     PointField("y", 4, PointField.FLOAT32, 1),
@@ -17,14 +17,14 @@ fields = [
 # Create a struct corresponding to the fields we defined above:
 # One float each for x,y,z and one byte each for r,g,b,a
 point_struct = struct.Struct("<fffBBBB")
-​
+
 def color(point: Point, t: float):
     r = 0.5 + 0.5 * point.x / 20
     g = point.y / 20
     b = 0.5 + 0.5 * math.sin(t)
     a = 0.5 + 0.5 * ((point.x / 20) * (point.y / 20))
     return int(r * 255), int(g * 255), int(b * 255), int(a * 255)
-​
+
 def make_point_cloud(stamp: Time):
     # Create a 20x20 grid of points that move around over time
     t = stamp.to_sec()
@@ -48,7 +48,7 @@ def make_point_cloud(stamp: Time):
         row_step=len(buffer),
         data=buffer,
     )
-​
+
 with open(sys.argv[1], "wb") as f:
     ros_writer = Writer(f)
     for i in range(0, 60 * 10):
